bad8daa22cd8af333c8dd3aeddd2ed10231edf0c
Ajithmukka <mukkaajith@gmail.com>
2025-07-25 11:11:24 +0530

    changes

--- Backend/db.js
+++ Backend/db.js
@@ -1,11 +1,12 @@
-const { Pool } = require("pg");
+const { Pool } = require('pg');
+require('dotenv').config();
 
 const pool = new Pool({
   user: "postgres",          
   host: "localhost",
-  database: "student_db",
-  password: "postgres",
-  port: 5432,
+  database: "student_db",    
+  password: "postgres", 
+  port: 5432
 });
 
 module.exports = pool;
