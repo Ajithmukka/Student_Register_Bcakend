6074dcd24cb17102482c1bf454cb633757879136
Ajithmukka <mukkaajith@gmail.com>
2025-07-28 16:04:53 +0530

    changes in studentController.js

--- Backend/controllers/studentController.js
+++ Backend/controllers/studentController.js
@@ -4,7 +4,7 @@ exports.registerStudent = async (req, res) => {
   const { name, email, password, mobile, course } = req.body;
   try {
     const result = await pool.query(
-      "INSERT INTO students (name, email, password, mobile, course) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      "INSERT INTO student (name, email, password, mobile, course) VALUES ($1, $2, $3, $4, $5) RETURNING *",
       [name, email, password, mobile, course]
     );
     res.json({ message: "Student registered", student: result.rows[0] });
